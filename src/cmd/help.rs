720f67e88919f9572215a044b52470d4b23e2ad5
FabianSolheim <fabian.j.solheim@gmail.com>
2024-02-10 23:50:58 +0100

    add: init usage to help cmd

--- src/cmd/help.rs
+++ src/cmd/help.rs
@@ -10,6 +10,7 @@ const FLAIR: &str = r#"
 pub fn help() {
     println!("\x1b[34m{} \x1b[0m", FLAIR);
     println!("Usage:");
+    println!("    \x1b[34mflair init \x1b[0m- Initialize a new migration directory");
     println!("    \x1b[34mflair create <name> \x1b[0m- Create a new migration file");
     println!("    \x1b[34mflair up \x1b[0m- Run all pending migrations");
     println!("    \x1b[34mflair down \x1b[0m- Reverts last applied migration");
